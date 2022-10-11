fd5dc52613fb4a8e429c11e6daef665ebe4d6f7a
Mauro <maurogutierreztormo@gmail.com>
2022-10-11 10:14:25 +0200

    añadi mi edad y mi fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,10 +1,11 @@
-public class Prueba.java {
+public class Prueba	{
 
 
 	public static void main(String args[]) {
 
 		
 		System.out.println("Gutierrez Tormo");
+		System.out.println("17 años y naci el 16-11-2004");
 		
 
 	
